3d054ef5e7213c0c3bbe8d03489bba90c24cc13c
Utkarsh-2525 <2101641530158@psit.ac.in>
2024-07-23 12:28:55 +0530

    Social Media Handler page added

---
+++ Pages/Scripts/script.js
@@ -0,0 +1,32 @@
+let customSelects = document.querySelectorAll('.custom-select');
+ 
+// Attach click event listeners to each custom select
+customSelects.forEach(function (select) {
+    let selectSelected = select.querySelector('.select-selected');
+    let selectItems = select.querySelector('.select-items');
+    let options = selectItems.querySelectorAll('div');
+ 
+    // Toggle the dropdown visibility when the select box is clicked
+    selectSelected.addEventListener('click', function () {
+        if (selectItems.style.display === 'block') {
+            selectItems.style.display = 'none';
+        } else {
+            selectItems.style.display = 'block';
+        }
+    });
+ 
+    // Set the selected option and hide the dropdown when an option is clicked
+    options.forEach(function (option) {
+        option.addEventListener('click', function () {
+            selectSelected.textContent = option.textContent + " ????";
+            selectItems.style.display = 'none';
+        });
+    });
+ 
+    // Close the dropdown if the user clicks outside of it
+    window.addEventListener('click', function (e) {
+        if (!select.contains(e.target)) {
+            selectItems.style.display = 'none';
+        }
+    });
+});
